b27ad88b7561461fe68cd81dce40d7d5b9824af1
joaopedrobarros777 <jpedrocbarros@gmail.com>
2021-05-07 17:21:49 -0300

    uploading

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Flex } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Flex 
+      w="100vw" 
+      h="100vw" 
+      align="center" 
+      justify="center"
+    >
+      <h1>Hello</h1>
+    </Flex>
+  )
+}
