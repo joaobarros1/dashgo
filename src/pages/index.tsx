72712ca0f28c77cc8a485eaa6c995517b5d76cd1
joaopedrobarros777 <jpedrocbarros@gmail.com>
2021-05-10 01:24:25 -0300

    dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,29 @@
-import { Flex, Input, Button } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
 export default function Home() {
   return (
-    <Flex 
-      w="100vw" 
-      h="100vw" 
-      align="center" 
+    <Flex
+      w="100vw"
+      h="100vw"
+      align="center"
       justify="center"
     >
-      <Flex 
-        as="form" 
-        width="100%" 
+      <Flex
+        as="form"
+        width="100%"
         maxWidth={360}
         bg="gray.800"
         p="8"
         borderRadius={8}
         flexDir="column"
       >
-        <Input name="email" type="email" />
-        <Input name="password" type="password" />
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+        <Button type="submit" mt="6" colorScheme="pink" size="lg">Entrar</Button>
       </Flex>
     </Flex>
   )
